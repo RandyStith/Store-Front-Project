9e2e6ea1e16159a90f34fa8ed4c6d2404ad9a0db
Randy Stith <225rstith@frhsd.com>
2024-02-28 11:48:54 -0500

    almost done with addtocart

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-var removeCartItemButtons = document.getElementsByClassName("button1");
+if(document.readyState == 'loading') {
+    document.addEventListener('DOMContentloaded', ready)
+} else {
+    ready()
+}
+function ready() {
+    var removeCartItemButtons = document.getElementsByClassName("button1");
 for (var i = 0; i < removeCartItemButtons.length; i++) {
     var button = removeCartItemButtons[i]
     button.addEventListener('click', function(event) {
@@ -7,10 +13,27 @@ for (var i = 0; i < removeCartItemButtons.length; i++) {
         updateCartTotal()
     } )
 }
+var addToCartButtons = document.getElementsByClassName('cartbtn')
+    for (var i = 0; i < addToCartButtons.length; i++) {
+        var button = addToCartButtons[i]
+        button.addEventListener('click', addToCartClicked)
+    }
+}
+
+function addToCartClicked(event) {
+    var button = event.target
+    var shopItem = button.parentElement.parentElement
+    var title = getElementsByClassName('title')[0].innertext
+    var price = shopItem.getElementsByClassName('price1')[0].innertext
+    console.log(title, price)
+}
+
+
+
 
 function updateCartTotal() {
-    var cartItemContainer = document.getElementsByClassName('container')[0]
-    var cartRows = cartItemContainer.getElementsByClassName('row')
+    var cartItemContainer = document.getElementsByClassName('container1')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('rows')
     var total = 0
     for (var i = 0; i < cartRows.length; i++) {
         var cartRow = cartRows[i]
@@ -18,6 +41,6 @@ function updateCartTotal() {
         var price = parseFloat(priceElement.innerText.replace('$', ''))
         total = total + price 
     }
-    document.getElementsByClassName('total')[0].innerText = total
+    document.getElementsByClassName('total')[0].innerText = `$${total}`
 
 }
