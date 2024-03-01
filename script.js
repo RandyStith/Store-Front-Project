c565b7d716ec2223c55066ddb2c9b2320838fbbb
Randy Stith <225rstith@frhsd.com>
2024-03-01 10:39:39 -0500

    almost done with adding to cart

--- script.js
+++ script.js
@@ -3,33 +3,37 @@ if(document.readyState == 'loading') {
 } else {
     ready()
 }
+
 function ready() {
     var removeCartItemButtons = document.getElementsByClassName("button1");
 for (var i = 0; i < removeCartItemButtons.length; i++) {
     var button = removeCartItemButtons[i]
-    button.addEventListener('click', function(event) {
-        var buttonClicked = event.target
-        buttonClicked.parentElement.remove()
-        updateCartTotal()
-    } )
-}
-var addToCartButtons = document.getElementsByClassName('cartbtn')
+    button.addEventListener('click', removeCartItem) 
+    } 
+
+    var addToCartButtons = document.getElementsByClassName('cartbtn')
     for (var i = 0; i < addToCartButtons.length; i++) {
         var button = addToCartButtons[i]
         button.addEventListener('click', addToCartClicked)
     }
 }
 
+
+function removeCartItem(event) {
+    var buttonClicked = event.target
+        buttonClicked.parentElement.remove()
+        updateCartTotal()
+}
+
 function addToCartClicked(event) {
     var button = event.target
     var shopItem = button.parentElement.parentElement
-    var title = getElementsByClassName('title')[0].innertext
-    var price = shopItem.getElementsByClassName('price1')[0].innertext
-    console.log(title, price)
+    var title = shopItem.getElementsByClassName('title')[0].innertext
+    var price1 = shopItem.getElementsByClassName('price1')[0].innertext
+    var imageSrc = shopItem.getElementsByClassName('image')[0].src
+    console.log(title, price1, imageSrc)
+    addItemToCart(title, price1, imageSrc)
 }
-
-
-
 
 function updateCartTotal() {
     var cartItemContainer = document.getElementsByClassName('container1')[0]
@@ -41,6 +45,17 @@ function updateCartTotal() {
         var price = parseFloat(priceElement.innerText.replace('$', ''))
         total = total + price 
     }
-    document.getElementsByClassName('total')[0].innerText = `$${total}`
+    document.getElementsByClassName('total')[0].innerText = '$' + total
 
 }
+
+function addItemToCart(title, price, imageSrc) {
+    var cartRow = document.createElement(div)
+    cartRow.innertext = title
+    var cartItems = document.getElementsByClassName(container1)
+    cartItems.append(cartRow)
+}
+
+
+
+
